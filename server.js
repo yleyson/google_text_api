95c7f2e11a258e090c2915423135ebfcaf66f834
yleyson <yuvalleyson@gmail.com>
2022-07-06 13:32:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,19 @@ const helmet = require('helmet');
 
 //port
 const PORT = process.env.PORT || 5008;
-const bodyParser = require('body-parser')
+
 //create server
 const server = express();
 server.use(express.json()); //enable json support
 server.use(cors()); //enable global access
 server.use(helmet()); //more defense
 
-server.use(bodyParser.json({
+server.use(express.json({
     limit: '50mb'
 }));
 
-server.use(bodyParser.urlencoded({
-    limit: '50mb',
-    parameterLimit: 100000,
-    extended: true
+server.use(express.urlencoded({
+    limit: '50mb'
 }));
 
 server.use('/api/google_text', require('./controllers/api_controller'));
